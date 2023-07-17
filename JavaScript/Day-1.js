03f3889b917d53589c1ca054b45fbe48acc264a1
obe1datt <75526039+obe1datt@users.noreply.github.com>
2023-07-17 17:29:35 +0300

    Create Day-1.js

---
+++ JavaScript/Day-1.js
@@ -0,0 +1,51 @@
+// chosr h1 elemant and make it blue color 
+// document.querySelector("h1").style.color = "blue" ;
+/*
+Multi Line Comment 
+*/
+// console.log("Write output using log metohd");
+// document.write("Write Putput using write metihd");
+// window.alert("Wite output using  alert");
+// window.prompt("Write Ooutput using propmt");
+// window.alert("XSS from Javascript");
+// document.write("Mohammad Qeis Obeidat ") 
+// document.write("<h2>Hello <span> Page </span> </h2>") 
+// console.log("Hi Log")
+
+
+// console.log("LOG");
+// console.error("Log");
+// console.table(["Mo" , "Hadi" , "ALI"]);
+// console.log("HI from %c js file", "color:red ; font-size:40px");
+
+
+/*
+ Data Types Intro
+*/
+
+// console.log("Mohmammad Qeis");
+// console.log(typeof("Mohmammad Qeis")); //string  ;
+// console.log(typeof 3000); // number  
+// console.log(typeof 32232.12); //number  
+// console.log(typeof  [1,2,3,4,5]); //array object 
+// console.log(typeof ["Mo","HI" ,"ALI"]) ; // array object 
+// console.log(typeof  {MOH:"JO",age:17,contry:"JO"}) // object 
+// console.log(typeof true) ; // boolean 
+// console.log(typeof undefined) ;// undifend
+// console.log(typeof null); //object 
+
+
+
+/*
+Varible 
+*/
+
+var  name = 'Mohammad Qeis' ; 
+var  age  = 22  ;  
+//or 
+var name1  = "Hadi obeidta",
+     age=19;
+
+console.log(name);
+console.log(hi); //from the id of html 
+hi.innerHTML = "0be1dat"; 
